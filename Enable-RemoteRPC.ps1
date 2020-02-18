3909e7b46ff6429a65ac522168e7f6a64f2b0ae6
FingersOnFire <fingersonfirecom+github@gmail.com>
2020-02-18 11:49:13 +0100

    Create Enable-RemoteRPC.ps1

---
+++ Enable-RemoteRPC.ps1
@@ -0,0 +1,12 @@
+﻿$ComputerName = '' #replace with the computer name
+$LMtype = [Microsoft.Win32.RegistryHive]::LocalMachine
+$LMkey = "SYSTEM\CurrentControlSet\Control\Terminal Server"
+$LMRegKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($LMtype,$ComputerName)
+$regKey = $LMRegKey.OpenSubKey($LMkey,$true)
+If($regKey.GetValue("AllowRemoteRPC") -ne 1)
+{
+    $regKey.SetValue("AllowRemoteRPC",1)
+    Start-Sleep -Seconds 1
+}
+$regKey.Dispose()
+$LMRegKey.Dispose()
